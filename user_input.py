a7436fc830da0160bc6ba1c8257175eccf544241
Kamau Nyambura <kanja.nyambura.kamau@gmail.com>
2024-02-23 23:22:25 +0300

    Initial commit

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input('Enter your name:')
+age = input('Enter your age:')
+location = input('Enter where you currently reside:')
+print(f'Good job {name}! {age} years is a great place to be at. I hope you are loving it in {location}. Stay safe always!')
